f25ca4f9476b3c46f498e766c998c6c88c81b729
Pedro Soares <pedrinhocef@gmail.com>
2018-03-21 20:13:39 -0300

    criando pagamento activity

---
+++ app/src/main/java/br/com/bixtrader/bixviagens/ui/activity/PagamentoActivity.java
@@ -0,0 +1,15 @@
+package br.com.bixtrader.bixviagens.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import br.com.bixtrader.bixviagens.R;
+
+public class PagamentoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pagamento);
+    }
+}
